2de7f1c7d39929daa25eb1d08bbc0a7e4fa85646
Elanju1006 <elanpersonal8@gmail.com>
2022-03-13 08:55:19 +0530

    Fifth Commit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,18 @@
 function urlConstruct(){
+    var name=document.getElementById("name").value;
+    var number=document.getElementById("phoneNumber").value;
+    var date=document.getElementById("dob").value;
+    var email=document.getElementById("email").value;
+    var Designation=document.getElementById("Designation").value;
+    var url=new URL("http://127.0.0.1:5501/Views/result.html");
+    url.searchParams.append("name",name);
+    url.searchParams.append("phoneNumber",number);
+    url.searchParams.append("dob",date);
+    url.searchParams.append("email",email);
+    url.searchParams.append("Designation",Designation);
+    window.location.href=url;
     
-}
+  }
 
 function display(){
     const urlParams = new URLSearchParams(window.location.search);
